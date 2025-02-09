0327b2c5a8633fe0fb2d60dec926dba08090d9b2
Hussain Yousof <mhussainyousof@gmail.com>
2025-02-09 18:27:43 +0330

    Initialize project structure and models

---
+++ android/app/src/main/kotlin/com/example/popcornhub/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.popcornhub
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/data/core/api_constants.dart
@@ -0,0 +1,8 @@
+class ApiConstants {
+   ApiConstants._();
+   
+  static const String BASE_URL = 'https://api.themoviedb.org/3/';
+  static const String API_KEY = '8664bb4df526b5a41f998fa7c7e9a836';
+  static const String BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
+
+}
