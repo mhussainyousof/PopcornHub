48f31e470fb893516dcd243a85cf0e100968ffd6
Hussain Yousof <mhussainyousof@gmail.com>
2025-02-23 18:26:03 +0330

    delete unnecessary codes

--- lib/common/extensions/size_extension.dart
+++
@@ -1,7 +0,0 @@
-// import 'package:popcornhub/common/screenutil/screenutil.dart';
-
-// extension SizeExtension on num{
-//   double get w => ScreenUtil().setWidth(this);
-//   double get h => ScreenUtil().setHeight(this);
-//   double get sp => ScreenUtil().setSp(this);
-// }
